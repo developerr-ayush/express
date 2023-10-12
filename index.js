53f63debee38c28f4a01f0655e7e9cc22fe4fa11
shahayush480 <shahayush480@gmail.com>
2023-10-12 07:00:23 +0530

    express js part 3

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const data = require("./data.json");
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index.ejs");
+  // extention is not required but file in views folder should have ejs extention
+});
+app.get("/instagram/user/:userid", (req, res) => {
+  let { userid } = req.params;
+  res.render("user.ejs", { data: data[userid] });
   // extention is not required but file in views folder should have ejs extention
 });
 app.set("views", path.join(__dirname, "/views"));
