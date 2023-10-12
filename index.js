6d306991708e41875f6726a864b18ab191bd2db5
shahayush480 <shahayush480@gmail.com>
2023-10-12 06:26:26 +0530

    changes

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+  // extention is not required but file in views folder should have ejs extention
+});
+app.set("views", path.join(__dirname, "/views"));
